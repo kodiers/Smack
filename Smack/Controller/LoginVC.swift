6c7ae70fba4f943c9743ece93c53b1c82ae1265b
Victor Yamchinov <kodiers@gmail.com>
2018-03-14 03:41:49 +0300

    added create accountvc

--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -36,4 +36,8 @@ class LoginVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func createAccountBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
+    
 }
