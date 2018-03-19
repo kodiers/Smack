bd4dd6d567d7c0a383bb847622dbaab996b91c10
Victor Yamchinov <kodiers@gmail.com>
2018-03-19 04:16:02 +0300

    work on register functionality + some improvements

---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,41 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Viktor Yamchinov on 19/03/2018.
+//  Copyright © 2018 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+
+}
