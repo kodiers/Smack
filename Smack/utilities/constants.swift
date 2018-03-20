fbade467db5da1a97b9d72f1a2beb2cbc0b7dda2
Victor Yamchinov <kodiers@gmail.com>
2018-03-20 04:04:03 +0300

    added login user request

--- Smack/utilities/constants.swift
+++ Smack/utilities/constants.swift
@@ -13,6 +13,10 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 // URL Constants
 let BASE_URL = "http://localhost:3005/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+
+//Headers
+let HEADER = ["Content-Type": "application/json; charset=utf-8"]
 
 // Segues
 let TO_LOGIN = "toLogin"
