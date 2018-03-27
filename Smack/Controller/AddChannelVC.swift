30d9599b8c475872358844ab5bef13cba36f7edc
Victor Yamchinov <kodiers@gmail.com>
2018-03-28 02:05:43 +0300

    added AddChannelVC

---
+++ Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,58 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Viktor Yamchinov on 28/03/2018.
+//  Copyright © 2018 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setUpView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func setUpView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameText.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor: SMACK_PURPLE_PLACEHOLDER])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor: SMACK_PURPLE_PLACEHOLDER])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+}
--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -91,5 +91,10 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
     }
     
+    @IBAction func addChannelPressed(_ sender: Any) {
+        let addChannel = AddChannelVC()
+        addChannel.modalPresentationStyle = .custom
+        present(addChannel, animated: true, completion: nil)
+    }
     
 }
